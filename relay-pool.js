0808671e0716446ffdf3dc393a62cfcc00e4ed30
Vitor Pamplona <vitor@vitorpamplona.com>
2024-08-08 15:25:23 -0400

    pool calls close instead of using the inner calls

--- relay-pool.js
+++ relay-pool.js
@@ -63,7 +63,7 @@ RelayPool.prototype.remove = function relayPoolRemove(url) {
 
 	for (const relay of this.relays) {
 		if (relay.url === url) {
-			relay.ws && relay.ws.close()
+			relay.close()
 			this.relays = this.replays.splice(i, 1)
 			return true
 		}
