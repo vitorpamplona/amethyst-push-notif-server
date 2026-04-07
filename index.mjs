bf4e3286a8d621b639c7235bc1530262db9e3ec6
Vitor Pamplona <vitor@vitorpamplona.com>
2026-04-07 15:46:16 -0400

    checks if it was sent before

--- index.mjs
+++ index.mjs
@@ -293,12 +293,13 @@ async function restartRelayPool() {
     relayPool.on('event', (relay, sub_id, ev) => {
         if (relay.isLive) {
             try {
+                if (sentCache.has(ev.id)) return
+
                 const nowUnix = Math.floor(Date.now() / 1000)
                 if (
                     ((ev.kind == 4 || ev.kind == 9735 || ev.kind == 21059) && ev.created_at > nowUnix - 30) ||
                     (ev.kind == 1059 && ev.created_at > nowUnix - 172800)
                 ) {
-                    if (sentCache.has(ev.id)) return
                     sentCache.set(ev.id, ev.id)
 
                     notify(ev, relay)
